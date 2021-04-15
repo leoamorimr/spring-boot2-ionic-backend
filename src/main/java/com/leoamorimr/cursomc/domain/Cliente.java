7a0146823508c7a6f741107930d424e3c549a412
leoamorimr <leoamorimr@gmail.com>
2021-04-15 16:26:54 -0300

    Endpoint /clientes/{id} Disponível!

--- src/main/java/com/leoamorimr/cursomc/domain/Cliente.java
+++ src/main/java/com/leoamorimr/cursomc/domain/Cliente.java
@@ -14,183 +14,187 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.leoamorimr.cursomc.domain.enums.TipoCliente;
 
 @Entity
 public class Cliente implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer cod;
-    private String nome;
-    private String email;
-    private String cpf;
-    private Integer tipoCliente;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer cod;
+	private String nome;
+	private String email;
+	private String cpf;
+	private Integer tipoCliente;
 
-    @OneToMany(mappedBy = "cliente")
-    private List<Endereco> enderecos = new ArrayList<>();
+	@OneToMany(mappedBy = "cliente")
+	private List<Endereco> enderecos = new ArrayList<>();
 
-    /*
-     * Collection do tipo Set não permite repetição de valores. Não é necessário
-     * criar uma classe Telefone neste caso só para implementar o telefone.
-     */
-    @ElementCollection
-    @CollectionTable(name = "TELEFONE")
-    private Set<String> telefones = new HashSet<>();
+	/*
+	 * Collection do tipo Set não permite repetição de valores. Não é necessário
+	 * criar uma classe Telefone neste caso só para implementar o telefone.
+	 * 
+	 * @JsonManagedReference para não ocorrer erro de referencia cíclica no Json.
+	 */
+	@JsonManagedReference
+	@ElementCollection
+	@CollectionTable(name = "TELEFONE")
+	private Set<String> telefones = new HashSet<>();
 
-    /**
-     * 
-     */
-    public Cliente() {
-    }
+	/**
+	 * 
+	 */
+	public Cliente() {
+	}
 
-    /**
-     * @param cod
-     * @param nome
-     * @param email
-     * @param cpf
-     * @param tipoCliente
-     */
-    public Cliente(Integer cod, String nome, String email, String cpf, TipoCliente tipoCliente) {
-        this.cod = cod;
-        this.nome = nome;
-        this.email = email;
-        this.cpf = cpf;
-        this.tipoCliente = tipoCliente.getCod();
-    }
+	/**
+	 * @param cod
+	 * @param nome
+	 * @param email
+	 * @param cpf
+	 * @param tipoCliente
+	 */
+	public Cliente(Integer cod, String nome, String email, String cpf, TipoCliente tipoCliente) {
+		this.cod = cod;
+		this.nome = nome;
+		this.email = email;
+		this.cpf = cpf;
+		this.tipoCliente = tipoCliente.getCod();
+	}
 
-    /**
-     * @return the cod
-     */
-    public Integer getCod() {
-        return cod;
-    }
+	/**
+	 * @return the cod
+	 */
+	public Integer getCod() {
+		return cod;
+	}
 
-    /**
-     * @param cod the cod to set
-     */
-    public void setCod(Integer cod) {
-        this.cod = cod;
-    }
+	/**
+	 * @param cod the cod to set
+	 */
+	public void setCod(Integer cod) {
+		this.cod = cod;
+	}
 
-    /**
-     * @return the nome
-     */
-    public String getNome() {
-        return nome;
-    }
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
 
-    /**
-     * @param nome the nome to set
-     */
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    /**
-     * @return the email
-     */
-    public String getEmail() {
-        return email;
-    }
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
 
-    /**
-     * @param email the email to set
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    /**
-     * @return the cpf
-     */
-    public String getCpf() {
-        return cpf;
-    }
+	/**
+	 * @return the cpf
+	 */
+	public String getCpf() {
+		return cpf;
+	}
 
-    /**
-     * @param cpf the cpf to set
-     */
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
+	/**
+	 * @param cpf the cpf to set
+	 */
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
 
-    /**
-     * @return the tipoCliente
-     */
-    public TipoCliente getTipoCliente() {
-        return TipoCliente.toEnum(tipoCliente);
-    }
+	/**
+	 * @return the tipoCliente
+	 */
+	public TipoCliente getTipoCliente() {
+		return TipoCliente.toEnum(tipoCliente);
+	}
 
-    /**
-     * @param tipoCliente the tipoCliente to set
-     */
-    public void setTipoCliente(TipoCliente tipoCliente) {
-        this.tipoCliente = tipoCliente.getCod();
-    }
+	/**
+	 * @param tipoCliente the tipoCliente to set
+	 */
+	public void setTipoCliente(TipoCliente tipoCliente) {
+		this.tipoCliente = tipoCliente.getCod();
+	}
 
-    /**
-     * @return the enderecos
-     */
-    public List<Endereco> getEnderecos() {
-        return enderecos;
-    }
+	/**
+	 * @return the enderecos
+	 */
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
 
-    /**
-     * @param enderecos the enderecos to set
-     */
-    public void setEnderecos(List<Endereco> enderecos) {
-        this.enderecos = enderecos;
-    }
+	/**
+	 * @param enderecos the enderecos to set
+	 */
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}
 
-    /**
-     * @return the telefones
-     */
-    public Set<String> getTelefones() {
-        return telefones;
-    }
+	/**
+	 * @return the telefones
+	 */
+	public Set<String> getTelefones() {
+		return telefones;
+	}
 
-    /**
-     * @param telefones the telefones to set
-     */
-    public void setTelefones(Set<String> telefones) {
-        this.telefones = telefones;
-    }
+	/**
+	 * @param telefones the telefones to set
+	 */
+	public void setTelefones(Set<String> telefones) {
+		this.telefones = telefones;
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.lang.Object#hashCode()
-     */
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + cod;
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + cod;
+		return result;
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.lang.Object#equals(java.lang.Object)
-     */
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Cliente other = (Cliente) obj;
-        if (cod != other.cod)
-            return false;
-        return true;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (cod != other.cod)
+			return false;
+		return true;
+	}
 
 }
---
+++ src/main/java/com/leoamorimr/cursomc/service/ClienteService.java
@@ -0,0 +1,24 @@
+package com.leoamorimr.cursomc.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leoamorimr.cursomc.domain.Cliente;
+import com.leoamorimr.cursomc.exception.ObjectNotFoundException;
+import com.leoamorimr.cursomc.repository.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+	private ClienteRepository repo;
+
+    public Cliente buscar(Integer id) {
+        Optional<Cliente> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+    }
+
+}
