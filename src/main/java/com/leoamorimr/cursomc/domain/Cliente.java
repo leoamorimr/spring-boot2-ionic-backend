6134a6d962ec84af38a42d732a9e7ac497c69ef5
leoamorimr <leoamorimr@gmail.com>
2021-04-16 14:49:39 -0300

    Remoção da anotação @JsonManagedReference e alteração da @JsonBackReferente por @JsonIgnore

--- src/main/java/com/leoamorimr/cursomc/domain/Cliente.java
+++ src/main/java/com/leoamorimr/cursomc/domain/Cliente.java
@@ -14,7 +14,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.leoamorimr.cursomc.domain.enums.TipoCliente;
 
@@ -45,7 +44,7 @@ public class Cliente implements Serializable {
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<Pedido>();
 
