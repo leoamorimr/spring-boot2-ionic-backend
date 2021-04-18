0f8feb6a62509711a8d9263422c92fd49207d67c
Leonardo Amorim Ramos <leoamorimr@gmail.com>
2021-04-18 14:15:51 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/leoamorimr/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/leoamorimr/cursomc/resource/CategoriaResource.java
@@ -1,26 +1,39 @@
 package com.leoamorimr.cursomc.resource;
 
-import com.leoamorimr.cursomc.domain.Categoria;
-import com.leoamorimr.cursomc.service.CategoriaService;
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.leoamorimr.cursomc.domain.Categoria;
+import com.leoamorimr.cursomc.service.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-    @Autowired
-    private CategoriaService service;
+	@Autowired
+	private CategoriaService service;
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<?> find(@PathVariable Integer id) {
-        Categoria obj = service.buscar(id);
-        return ResponseEntity.ok().body(obj);
-    }
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+
+		// fromCurrentRequest pega a URI da requisição(http://localhost:8080/categorias)
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 
 }
--- src/main/java/com/leoamorimr/cursomc/service/CategoriaService.java
+++ src/main/java/com/leoamorimr/cursomc/service/CategoriaService.java
@@ -2,23 +2,28 @@ package com.leoamorimr.cursomc.service;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.leoamorimr.cursomc.domain.Categoria;
 import com.leoamorimr.cursomc.exception.ObjectNotFoundException;
 import com.leoamorimr.cursomc.repository.CategoriaRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 @Service
 public class CategoriaService {
 
-    @Autowired
-    private CategoriaRepository repo;
+	@Autowired
+	private CategoriaRepository repo;
 
-    public Categoria buscar(Integer id) {
-        Optional<Categoria> obj = repo.findById(id);
-        return obj.orElseThrow(() -> new ObjectNotFoundException(
-                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
-    }
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
 
 }
