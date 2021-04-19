c51055c4fc97bbc285337c0f3ab1f171061a3992
leoamorimr <leoamorimr@gmail.com>
2021-04-19 17:10:51 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/leoamorimr/cursomc/service/CategoriaService.java
+++ src/main/java/com/leoamorimr/cursomc/service/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.leoamorimr.cursomc.domain.Categoria;
+import com.leoamorimr.cursomc.dto.CategoriaDTO;
 import com.leoamorimr.cursomc.repository.CategoriaRepository;
 import com.leoamorimr.cursomc.service.exception.DataIntegrityException;
 import com.leoamorimr.cursomc.service.exception.ObjectNotFoundException;
@@ -61,5 +62,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
