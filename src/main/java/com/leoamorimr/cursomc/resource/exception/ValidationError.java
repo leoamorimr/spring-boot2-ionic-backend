eec3dc4229085bbc4f765dae7d07ae1cd662f9d6
leoamorimr <leoamorimr@gmail.com>
2021-04-19 17:30:31 -0300

    Personalizando o retorno da validação

---
+++ src/main/java/com/leoamorimr/cursomc/resource/exception/ValidationError.java
@@ -0,0 +1,24 @@
+package com.leoamorimr.cursomc.resource.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	private static final long serialVersionUID = 1L;
+
+	private List<FieldMessage> errors = new ArrayList<>();
+
+	public ValidationError(Integer status, String msg, Long timesTamp) {
+		super(status, msg, timesTamp);
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String message) {
+		FieldMessage fieldMessage = new FieldMessage(fieldName, message);
+		errors.add(fieldMessage);
+	}
+
+}
