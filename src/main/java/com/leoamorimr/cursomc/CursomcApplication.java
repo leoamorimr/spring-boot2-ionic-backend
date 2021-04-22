9f3f661156b75baa8a0df0895712b5681d312f12
leoamorimr <leoamorimr@gmail.com>
2021-04-22 15:13:37 -0300

    .

--- src/main/java/com/leoamorimr/cursomc/CursomcApplication.java
+++ src/main/java/com/leoamorimr/cursomc/CursomcApplication.java
@@ -35,110 +35,110 @@ import com.leoamorimr.cursomc.repository.ProdutoRepository;
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	@Autowired
-	private ProdutoRepository produtoRepository;
-	@Autowired
-	private CidadeRepository cidadeRepository;
-	@Autowired
-	private EstadoRepository estadoRepository;
-	@Autowired
-	private ClienteRepository clienteRepository;
-	@Autowired
-	private EnderecoRepository enderecoRepository;
-	@Autowired
-	private PedidoRepository pedidoRepository;
-	@Autowired
-	private PagamentoRepository pagamentoRepository;
-	@Autowired
-	private ItemPedidoRepository itemPedidoRepository;
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+    @Autowired
+    private ProdutoRepository produtoRepository;
+    @Autowired
+    private CidadeRepository cidadeRepository;
+    @Autowired
+    private EstadoRepository estadoRepository;
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+    @Autowired
+    private PedidoRepository pedidoRepository;
+    @Autowired
+    private PagamentoRepository pagamentoRepository;
+    @Autowired
+    private ItemPedidoRepository itemPedidoRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(CursomcApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(CursomcApplication.class, args);
 
-	}
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		Categoria cat1 = new Categoria(null, "Informática");
-		Categoria cat2 = new Categoria(null, "Escritório");
-		Categoria cat3 = new Categoria(null, "Cama mesa e banho");
-		Categoria cat4 = new Categoria(null, "Eletrônicos");
-		Categoria cat5 = new Categoria(null, "Jardinagem");
-		Categoria cat6 = new Categoria(null, "Decoração");
-		Categoria cat7 = new Categoria(null, "Perfumaria");
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+        Categoria cat3 = new Categoria(null, "Cama mesa e banho");
+        Categoria cat4 = new Categoria(null, "Eletrônicos");
+        Categoria cat5 = new Categoria(null, "Jardinagem");
+        Categoria cat6 = new Categoria(null, "Decoração");
+        Categoria cat7 = new Categoria(null, "Perfumaria");
 
-		Produto p1 = new Produto(null, "Computador", 2000D);
-		Produto p2 = new Produto(null, "Impressora", 800D);
-		Produto p3 = new Produto(null, "Mouse", 80D);
+        Produto p1 = new Produto(null, "Computador", 2000D);
+        Produto p2 = new Produto(null, "Impressora", 800D);
+        Produto p3 = new Produto(null, "Mouse", 80D);
 
-		// Asssossiação da Categoria com o Produto
-		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
-		cat2.getProdutos().addAll(Arrays.asList(p2));
+        // Associação da Categoria com o Produto
+        cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+        cat2.getProdutos().add(p2);
 
-		// Associação do Produto com a Categoria
-		p1.getCategorias().addAll(Arrays.asList(cat1));
-		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
-		p3.getCategorias().addAll(Arrays.asList(cat1));
+        // Associação do Produto com a Categoria
+        p1.getCategorias().add(cat1);
+        p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+        p3.getCategorias().add(cat1);
 
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
-		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
+        produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
-		Estado est1 = new Estado(null, "Minas Gerais");
-		Estado est2 = new Estado(null, "São Paulo");
+        Estado est1 = new Estado(null, "Minas Gerais");
+        Estado est2 = new Estado(null, "São Paulo");
 
-		Cidade c1 = new Cidade(null, "Uberlândia", est1);
-		Cidade c2 = new Cidade(null, "São Paulo", est2);
-		Cidade c3 = new Cidade(null, "Campinas", est2);
+        Cidade c1 = new Cidade(null, "Uberlândia", est1);
+        Cidade c2 = new Cidade(null, "São Paulo", est2);
+        Cidade c3 = new Cidade(null, "Campinas", est2);
 
-		// Associação de Cidades com os Estados
-		est1.getCidades().addAll(Collections.singletonList(c1));
-		est2.getCidades().addAll(Arrays.asList(c2, c3));
+        // Associação de Cidades com os Estados
+        est1.getCidades().addAll(Collections.singletonList(c1));
+        est2.getCidades().addAll(Arrays.asList(c2, c3));
 
-		estadoRepository.saveAll(Arrays.asList(est1, est2));
-		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+        estadoRepository.saveAll(Arrays.asList(est1, est2));
+        cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "12313123", TipoCliente.PESSOAFISICA);
-		cli1.getTelefones().addAll(Arrays.asList("3222-2222", "9 99999-99"));
+        Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "12313123", TipoCliente.PESSOAFISICA);
+        cli1.getTelefones().addAll(Arrays.asList("3222-2222", "9 99999-99"));
 
-		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "32323232", cli1, c1);
-		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "sala 800", "Centro", "32323232", cli1, c2);
+        Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "32323232", cli1, c1);
+        Endereco e2 = new Endereco(null, "Avenida Matos", "105", "sala 800", "Centro", "32323232", cli1, c2);
 
-		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+        cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
 
-		// PERSISTENCIA DOS CLIENTE E ENDEREÇO
-		clienteRepository.save(cli1);
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+        // PERSISTENCIA DOS CLIENTE E ENDEREÇO
+        clienteRepository.save(cli1);
+        enderecoRepository.saveAll(Arrays.asList(e1, e2));
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
-		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 10:32"), cli1, e2);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+        Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 10:32"), cli1, e2);
 
-		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
-		ped1.setPagamento(pagto1);
+        Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+        ped1.setPagamento(pagto1);
 
-		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"),
-				null);
-		ped2.setPagamento(pagto2);
+        Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"),
+                null);
+        ped2.setPagamento(pagto2);
 
-		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+        cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
 
-		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
-		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+        pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+        pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 
-		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
-		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
-		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+        ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+        ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+        ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
 
-		ped1.getItems().addAll(Arrays.asList(ip1, ip2));
-		ped2.getItems().addAll(Arrays.asList(ip3));
+        ped1.getItems().addAll(Arrays.asList(ip1, ip2));
+        ped2.getItems().add(ip3);
 
-		p1.getItems().addAll(Arrays.asList(ip1));
-		p2.getItems().addAll(Arrays.asList(ip3));
-		p3.getItems().addAll(Arrays.asList(ip2));
+        p1.getItems().add(ip1);
+        p2.getItems().add(ip3);
+        p3.getItems().add(ip2);
 
-		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
-	}
+        itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
+    }
 
 }
