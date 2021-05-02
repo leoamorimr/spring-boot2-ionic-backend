da09fe1dc0c1e24f9591f1406b03f8723a6cb074
leoamorimr <leoamorimr@gmail.com>
2021-05-02 18:33:15 -0300

    Busca de produto por nome e categorias.

--- src/main/java/com/leoamorimr/cursomc/repository/PedidoRepository.java
+++ src/main/java/com/leoamorimr/cursomc/repository/PedidoRepository.java
@@ -1,9 +1,15 @@
 package com.leoamorimr.cursomc.repository;
 
+import com.leoamorimr.cursomc.domain.Categoria;
+import com.leoamorimr.cursomc.domain.Produto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.leoamorimr.cursomc.domain.Pedido;
+
+import java.util.List;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
---
+++ src/main/java/com/leoamorimr/cursomc/resource/utils/URL.java
@@ -0,0 +1,29 @@
+package com.leoamorimr.cursomc.resource.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class URL {
+
+    public static List<Integer> decodeIntList(String s) {
+        String[] vet = s.trim().split(",");
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < vet.length; i++) {
+            list.add(Integer.parseInt(vet[i]));
+        }
+//        return Arrays.asList(s.split(",")).stream().map(x->Integer.parseInt(x)).collect(Collectors.toList());
+        return list;
+    }
+
+    public static String decodeParam(String s) {
+        try {
+            return URLDecoder.decode(s, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            return "";
+        }
+    }
+}
