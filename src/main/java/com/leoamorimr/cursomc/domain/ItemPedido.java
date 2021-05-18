75aa0be84f09f9765f7536101193067c1c897433
leoamorimr <leoamorimr@gmail.com>
2021-05-18 15:37:23 +0100

    Subtotal e total do Pedido.

--- src/main/java/com/leoamorimr/cursomc/domain/ItemPedido.java
+++ src/main/java/com/leoamorimr/cursomc/domain/ItemPedido.java
@@ -105,4 +105,8 @@ public class ItemPedido implements Serializable {
 		return true;
 	}
 
+	public double getSubTotal(){
+		return (preco - desconto) * quantidade;
+	}
+
 }
--- src/main/java/com/leoamorimr/cursomc/domain/Pedido.java
+++ src/main/java/com/leoamorimr/cursomc/domain/Pedido.java
@@ -19,112 +19,121 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 public class Pedido implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
-	private Date instante;
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+    private Date instante;
 
-	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
-	private Pagamento pagamento;
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
+    private Pagamento pagamento;
 
-	@ManyToOne
-	@JoinColumn(name = "cliente_id")
-	private Cliente cliente;
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
 
-	@ManyToOne
-	@JoinColumn(name = "endereco_de_entrega_id")
-	private Endereco enderecoDeEntrega;
+    @ManyToOne
+    @JoinColumn(name = "endereco_de_entrega_id")
+    private Endereco enderecoDeEntrega;
 
-	@OneToMany(mappedBy = "id.pedido")
-	private Set<ItemPedido> items = new HashSet<>();
+    @OneToMany(mappedBy = "id.pedido")
+    private Set<ItemPedido> items = new HashSet<>();
 
-	public Pedido() {
-		super();
-	}
+    public Pedido() {
+        super();
+    }
 
-	public Pedido(Integer id, Date instante, Cliente cliente, Endereco enderecoDeEntrega) {
-		super();
-		this.id = id;
-		this.instante = instante;
-		this.cliente = cliente;
-		this.enderecoDeEntrega = enderecoDeEntrega;
-	}
+    public Pedido(Integer id, Date instante, Cliente cliente, Endereco enderecoDeEntrega) {
+        super();
+        this.id = id;
+        this.instante = instante;
+        this.cliente = cliente;
+        this.enderecoDeEntrega = enderecoDeEntrega;
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public Date getInstante() {
-		return instante;
-	}
+    public Date getInstante() {
+        return instante;
+    }
 
-	public void setInstante(Date instante) {
-		this.instante = instante;
-	}
+    public void setInstante(Date instante) {
+        this.instante = instante;
+    }
 
-	public Pagamento getPagamento() {
-		return pagamento;
-	}
+    public Pagamento getPagamento() {
+        return pagamento;
+    }
 
-	public void setPagamento(Pagamento pagamento) {
-		this.pagamento = pagamento;
-	}
+    public void setPagamento(Pagamento pagamento) {
+        this.pagamento = pagamento;
+    }
 
-	public Cliente getCliente() {
-		return cliente;
-	}
+    public Cliente getCliente() {
+        return cliente;
+    }
 
-	public void setCliente(Cliente cliente) {
-		this.cliente = cliente;
-	}
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
 
-	public Endereco getEnderecoDeEntrega() {
-		return enderecoDeEntrega;
-	}
+    public Endereco getEnderecoDeEntrega() {
+        return enderecoDeEntrega;
+    }
 
-	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
-		this.enderecoDeEntrega = enderecoDeEntrega;
-	}
+    public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
+        this.enderecoDeEntrega = enderecoDeEntrega;
+    }
 
-	public Set<ItemPedido> getItems() {
-		return items;
-	}
+    public Set<ItemPedido> getItems() {
+        return items;
+    }
 
-	public void setItems(Set<ItemPedido> items) {
-		this.items = items;
-	}
+    public void setItems(Set<ItemPedido> items) {
+        this.items = items;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Pedido other = (Pedido) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Pedido other = (Pedido) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    public double getValorTotal() {
+        double soma = 0D;
+        for (ItemPedido ip : items) {
+            soma += ip.getSubTotal();
+        }
+        return soma;
+    }
+
 
 }
